7f4669dedc24eb33a491314786d3c204d44a6366
MLNavigator <brutbrulyako@gmail.com>
2024-03-29 16:39:36 +0300

    fix run

--- rebuild.sh
+++ rebuild.sh
@@ -4,5 +4,5 @@ docker stop chat_gpt_proxy_dc
 docker rm chat_gpt_proxy_dc
 docker rmi chat_gpt_proxy_dc
 docker build -t chat_gpt_proxy_dc .
-docker run -d --restart=always -e ACCESS_KEY=123456 -p 9081:9081 --name=chat_gpt_proxy_dc chat_gpt_proxy_dc
+docker run -d --restart=always -e ACCESS_KEY='123456' -p 9081:9081 --name=chat_gpt_proxy_dc chat_gpt_proxy_dc
 echo "finished - run container chat_gpt_proxy_dc"
